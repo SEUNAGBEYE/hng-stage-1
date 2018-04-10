8ccd23c17ced409c03ed37a52d2a7f777817cfd9
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-04-10 18:56:52 +0100

    complete task

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Hotels ng</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" media="screen" href="./public/css/main.css" />
+</head>
+<body>
+  <header>
+    <div class='fa fa-bars'></div>
+    <div id='home'>
+      <div>Hotels ng</div>
+      <div class='fa fa-home'></div>
+    <div>
+  </header>
+  <main>
+    <div id="image-container"></div>
+    <div>
+      <? echo date("h:i"); ?>
+    </div>
+  </main>
+</body>
+</html>
